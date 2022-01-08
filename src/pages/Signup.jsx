a994679e9ecebb082cc773d916fd159a91829aea
Mr developa <m.e.zacjac@gmail.com>
2022-01-08 10:10:24 +0000

    At first, i add issue with eslint but i manage to fix it by following this steps:
    1. Delete package-lock.json (not package.json!) and/or yarn.lock in your project folder.
    2. Delete node_modules in your project folder.
    3. Remove "babel-eslint" from dependencies and/or devDependencies in the package.json file in your project folder.
    4. Run npm install or yarn, depending on the package manager you us

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import HomeComponent from "../components/SignupComponent"
+export default function Signup() {
+    return (
+        <Layout>
+            <SignupComponent />
+        </Layout>
+    )
+}
